8faae045c5ba65052656a7239d4a96d81e2442ed
chenshi <756705113@qq.com>
2022-08-06 10:14:54 +0800

    系统监控-新增在线用户

--- src/views/main-sidebar-sub-menu.vue
+++ src/views/main-sidebar-sub-menu.vue
@@ -40,6 +40,8 @@ export default {
   },
   created () {
     this.$nextTick(() => {
+      let el = document.querySelector(".aui-wrapper")
+      if(el) el.style.setProperty("--themeColor", this.$store.state.theme)
       if (this.$refs.li) {
         let $li = this.$refs.li.$el
         let $a = $li.firstElementChild
