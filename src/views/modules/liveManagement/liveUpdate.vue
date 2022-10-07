398472eb6062af70536b3757e673fc607deb7ad4
chenshi <756705113@qq.com>
2022-10-07 16:40:21 +0800

    部分按钮增减防抖函数

--- src/views/modules/liveManagement/liveUpdate.vue
+++ src/views/modules/liveManagement/liveUpdate.vue
@@ -77,6 +77,7 @@
     </el-card>
 </template>
 <script>
+import debounce from "lodash/debounce"
 import CustomUpload from "@/components/common/custom-upload"
 import ComModule from "@/mixins/common-module"
 import ChooseAnchor from "@/components/chooseDialog/chooseAnchor"
@@ -235,7 +236,7 @@ export default {
 
         },
         // 表单提交
-        onSubmit() {
+        onSubmit: debounce(function() {
             this.$refs.dataForm.validate(async (valid) => {
                 if (valid) {
 
@@ -286,7 +287,7 @@ export default {
                     }
                 }
             })
-        }
+        }, 1500, { 'leading': true, 'trailing': false })
     }
 }
 </script>
