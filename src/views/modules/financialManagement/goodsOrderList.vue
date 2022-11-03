949b0ada62d88f205447a4a622f5825f564db340
haoyue <2696213429@qq.com>
2022-11-03 10:39:01 +0800

    商品订单列表新增结算价格列

--- src/views/modules/financialManagement/goodsOrderList.vue
+++ src/views/modules/financialManagement/goodsOrderList.vue
@@ -188,18 +188,6 @@
         >
         </el-table-column>
         <el-table-column
-          prop="settlementPrice"
-          label="APP结算价格"
-          header-align="center"
-          min-width="120px"
-          show-overflow-tooltip
-          align="center"
-        >
-          <template slot-scope="{ row }">
-            <span>{{ numberConvert(row.settlementPrice) }}</span>
-          </template>
-        </el-table-column>
-        <el-table-column
           prop="price"
           label="应付金额"
           header-align="center"
@@ -235,7 +223,7 @@
             <span>{{ numberConvert(row.realPrice) }}</span>
           </template>
         </el-table-column>
-        <!-- <el-table-column
+        <el-table-column
           prop="settlementPrice"
           label="结算价格"
           header-align="center"
@@ -246,7 +234,7 @@
           <template slot-scope="{ row }">
             <span>{{ numberConvert(row.settlementPrice) }}</span>
           </template>
-        </el-table-column> -->
+        </el-table-column>
         <el-table-column
           prop="payDate"
           label="支付完成时间"
