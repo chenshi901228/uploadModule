de0465624d1535f79ce75f972200f761d3fc4a2e
haoyue <2696213429@qq.com>
2022-12-16 14:43:21 +0800

    修改直播详情字段

--- src/views/modules/liveManagement/details/anchorInfo.vue
+++ src/views/modules/liveManagement/details/anchorInfo.vue
@@ -5,6 +5,9 @@
                 <el-form-item label="直播主题：" prop="liveTheme">
                     <span>{{ruleForm.liveTheme || '--'}}</span>
                 </el-form-item>
+                <el-form-item label="主播：" prop="anchorUser">
+                    <span>{{ruleForm.anchorUser}}</span>
+                </el-form-item>
                 <el-form-item label="开播时间：" prop="startDate">
                     <span>{{ruleForm.startDate || '--'}}</span>
                 </el-form-item>
